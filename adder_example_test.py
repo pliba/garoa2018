9f96c7325e022ea83664195158b2b258d488753d
Luciano Ramalho <luciano@ramalho.org>
2018-12-19 16:20:39 -0200

    Squashed 'prep/dia05/dialogue/' content from commit 32bf820
    git-subtree-dir: prep/dia05/dialogue
    git-subtree-split: 32bf820b3e847549036fd9af8c56f1723845ce7a

---
+++ adder_example_test.py
@@ -0,0 +1,41 @@
+from pytest import mark
+
+from dialogue import Dialogue
+
+from adder_example import adder
+
+
+@mark.parametrize("session", [
+    """
+    Enter 0 to quit.
+    [1] 0
+    """,
+    """
+    Enter 0 to quit.
+    [1] 1
+    1.0
+    """,
+    """
+    Enter 0 to quit.
+    [1] 11
+    11.0
+    [2] 22
+    33.0
+    [3] 33.9
+    66.9
+    """,
+    """
+    Enter 0 to quit.
+    [1] z
+    Please type numbers.
+    [1] 3
+    3.0
+    [2] 4
+    7.0
+    """,
+])
+def test_adder(capsys, session):
+    dlg = Dialogue(session)
+    adder(dlg.fake_input)
+    captured = capsys.readouterr()
+    assert dlg.session == captured.out
